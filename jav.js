0667f219b2063507b3dddc1f06b1e39c666ef040
betulesmakaniboz-engineercomputer <betulesmakaniboz@gmail.com>
2025-12-27 15:16:10 +0300

    Add files via upload

---
+++ jav.js
@@ -0,0 +1,10 @@
+document.getElementsByClassName("harfler")[0].style.fontSize="22px";
+document.getElementsByClassName("harfler")[1].style.fontSize="22px";
+document.getElementsByClassName("harfler")[0].style.color="white";
+document.getElementsByClassName("harfler")[1].style.color="white";
+var a = "1";
+var b = "98";
+var c = "8";
+var Toplam = a + b + c;
+alert(Toplam); 
+//betül esma kanıboz
